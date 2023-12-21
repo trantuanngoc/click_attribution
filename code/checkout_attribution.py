75ead73323bde5da87ba5fa40413a0dc6f889d31
Tuan-Ngoc Tran <ngocthoson1@gmail.com>
2023-12-21 15:40:04 +0700

    edit

--- code/checkout_attribution.py
+++ code/checkout_attribution.py
@@ -1,122 +1,132 @@
-from dataclasses import asdict, dataclass, field
-from typing import List, Tuple
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import (
+    IntegerType,
+    StringType,
+    StructType,
+    TimestampType,
+)
 
-from jinja2 import Environment, FileSystemLoader
+spark = SparkSession.builder.appName('example').getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
 
+click_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "clicks")
+    .option("startingOffsets", "earliest")
+    .load()
+)
 
+click_df1 = click_df.selectExpr("CAST(value AS STRING)", "timestamp")
 
+click_schema = (
+    StructType()
+    .add("click_id", StringType())
+    .add("user_id", StringType())
+    .add("product_id", StringType())
+    .add("product", StringType())
+    .add("price", StringType())
+    .add("url", StringType())
+    .add("user_agent", StringType())
+    .add("ip_address", StringType())
+    .add("datetime_occured", TimestampType())
+)
 
-@dataclass(frozen=True)
-class StreamJobConfig:
-    job_name: str = 'checkout-attribution-job'
-    jars: List[str] = field(default_factory=lambda: REQUIRED_JARS)
-    checkpoint_interval: int = 10
-    checkpoint_pause: int = 5
-    checkpoint_timeout: int = 5
-    parallelism: int = 2
+click_df2 = (
+    click_df1.withColumn("value", from_json(click_df1["value"], click_schema))
+    .select("value.*")
+    .alias('cl')
+    .withWatermark("datetime_occured", "15 seconds")
+    .createOrReplaceTempView("clicks")
+)
 
+checkout_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "checkouts")
+    .option("startingOffsets", "earliest")
+    .load()
+)
 
-@dataclass(frozen=True)
-class KafkaConfig:
-    connector: str = 'kafka'
-    bootstrap_servers: str = 'kafka:9092'
-    scan_stratup_mode: str = 'earliest-offset'
-    consumer_group_id: str = 'flink-consumer-group-1'
+checkout_df1 = checkout_df.selectExpr("CAST(value AS STRING)", "timestamp")
 
+checkout_schema = (
+    StructType()
+    .add("checkout_id", StringType())
+    .add("user_id", StringType())
+    .add("product_id", StringType())
+    .add("payment_method", StringType())
+    .add("total_amount", IntegerType())
+    .add("shipping_address", StringType())
+    .add("billing_address", StringType())
+    .add("user_agent", StringType())
+    .add("ip_address", StringType())
+    .add("datetime_occured", TimestampType())
+)
 
-@dataclass(frozen=True)
-class ClickTopicConfig(KafkaConfig):
-    topic: str = 'clicks'
-    format: str = 'json'
-
-
-@dataclass(frozen=True)
-class CheckoutTopicConfig(KafkaConfig):
-    topic: str = 'checkouts'
-    format: str = 'json'
-
-
-@dataclass(frozen=True)
-class ApplicationDatabaseConfig:
-    connector: str = 'jdbc'
-    url: str = 'jdbc:postgresql://postgres:5432/postgres'
-    username: str = 'postgres'
-    password: str = 'postgres'
-    driver: str = 'org.postgresql.Driver'
-
-
-@dataclass(frozen=True)
-class ApplicationUsersTableConfig(ApplicationDatabaseConfig):
-    table_name: str = 'commerce.users'
-
-
-@dataclass(frozen=True)
-class ApplicationAttributedCheckoutsTableConfig(ApplicationDatabaseConfig):
-    table_name: str = 'commerce.attributed_checkouts'
-
-
-def get_execution_environment(
-    config: StreamJobConfig,
-) -> Tuple[StreamExecutionEnvironment, StreamTableEnvironment]:
-    s_env = StreamExecutionEnvironment.get_execution_environment()
-    for jar in config.jars:
-        s_env.add_jars(jar)
-
-    s_env.enable_checkpointing(config.checkpoint_interval * 1000)
-    s_env.get_checkpoint_config().set_min_pause_between_checkpoints(
-        config.checkpoint_pause * 1000
+checkout_df2 = (
+    checkout_df1.withColumn(
+        "value", from_json(checkout_df1["value"], checkout_schema)
     )
-    s_env.get_checkpoint_config().set_checkpoint_timeout(
-        config.checkpoint_timeout * 1000
-    )
-    execution_config = s_env.get_config()
-    execution_config.set_parallelism(config.parallelism)
-    t_env = StreamTableEnvironment.create(s_env)
-    job_config = t_env.get_config().get_configuration()
-    job_config.set_string("pipeline.name", config.job_name)
-    return s_env, t_env
+    .select("value.*")
+    .alias('co')
+    .withWatermark("datetime_occured", "15 seconds")
+    .createOrReplaceTempView("checkouts")
+)
+
+database_url = "jdbc:postgresql://postgres:5432/postgres"
+
+user_df = (
+    spark.read.format('jdbc')
+    .option('url', database_url)
+    .option('dbtable', 'commerce.users')
+    .option('user', 'postgres')
+    .option('password', 'postgres')
+    .load()
+    .alias('u')
+    .createOrReplaceTempView("users")
+)
 
 
-def get_sql_query(
-    entity: str,
-    type: str = 'source',
-    template_env: Environment = Environment(loader=FileSystemLoader("code/")),
-) -> str:
-    config_map = {
-        'clicks': ClickTopicConfig(),
-        'checkouts': CheckoutTopicConfig(),
-        'users': ApplicationUsersTableConfig(),
-        'attributed_checkouts': ApplicationUsersTableConfig(),
-        'attribute_checkouts': ApplicationAttributedCheckoutsTableConfig(),
-    }
+enrich = spark.sql(
+    """
+    SELECT
+            co.checkout_id,
+            u.username AS user_name,
+            cl.click_id,
+            co.product_id,
+            co.payment_method,
+            co.total_amount,
+            co.shipping_address,
+            co.billing_address,
+            co.user_agent,
+            co.ip_address,
+            co.datetime_occured AS checkout_time,
+            cl.datetime_occured AS click_time
+        FROM
+            checkouts AS co
+            JOIN users AS u ON co.user_id = u.id
+            LEFT JOIN clicks AS cl ON co.user_id = cl.user_id
+            AND co.product_id = cl.product_id
+            AND co.datetime_occured BETWEEN cl.datetime_occured
+            AND cl.datetime_occured + INTERVAL '1' HOUR
+"""
+)
 
-    return template_env.get_template(f"{type}/{entity}.sql").render(
-        asdict(config_map.get(entity))
-    )
-
-
-def run_checkout_attribution_job(
-    t_env: StreamTableEnvironment,
-    get_sql_query=get_sql_query,
-) -> None:
-    t_env.execute_sql(get_sql_query('clicks'))
-    t_env.execute_sql(get_sql_query('checkouts'))
-    t_env.execute_sql(get_sql_query('users'))
-
-    t_env.execute_sql(get_sql_query('attributed_checkouts', 'sink'))
-
-    stmt_set = t_env.create_statement_set()
-    stmt_set.add_insert_sql(get_sql_query('attribute_checkouts', 'process'))
-
-    checkout_attribution_job = stmt_set.execute()
-    print(
-        f"""
-        Async attributed checkouts sink job
-         status: {checkout_attribution_job.get_job_client().get_job_status()}
-        """
-    )
+def foreach_batch_function(df, epoch_id):
+    df = df.dropDuplicates(['checkout_id'])
+    df.write.format("jdbc").mode('append').option(
+        "url", "jdbc:postgresql://postgres:5432/postgres"
+    ).option("dbtable", "commerce.attributed_checkouts").option(
+        "user", "postgres"
+    ).option(
+        "password", "postgres"
+    ).save()
 
 
-if __name__ == '__main__':
-    _, t_env = get_execution_environment(StreamJobConfig())
-    run_checkout_attribution_job(t_env)
+enrich.writeStream.foreachBatch(
+    foreach_batch_function
+    ).start().awaitTermination()
+
+
